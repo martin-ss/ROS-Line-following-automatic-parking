af0dff6c189bab7368a80b5ddb234a1f7348ece0
martin-ss <33465771+martin-ss@users.noreply.github.com>
2020-12-28 13:39:47 +0100

    Add files via upload

---
+++ Parking/AUTORACE/turtlebot3_autorace/turtlebot3_autorace/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mscv/catkin_ws/src/turtlebot3_autorace/turtlebot3_autorace
+# Build directory: /home/mscv/catkin_ws/src/turtlebot3_autorace/turtlebot3_autorace
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
